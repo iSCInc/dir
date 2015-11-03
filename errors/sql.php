2efbc42c1b2722fb63c4cb72b56a3ee062fed06c
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-11-03 16:55:56 +0100

    Create sql.php

---
+++ errors/sql.php
@@ -0,0 +1,12 @@
+<?php
+# SQL errors
+
+## To display SQL errors in error messages instead of "(SQL query hidden)", add the following to LocalSettings.php:
+
+$wgShowSQLErrors = true;
+$wgDebugDumpSql  = true;
+
+## You can also enable backtrace on SQL error by setting $wgShowDBErrorBacktrace:
+
+$wgShowDBErrorBacktrace = true;
+?>
